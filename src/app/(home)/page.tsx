1dcdc048483df11beaa203adde2171c8ef72bc05
okekefrancis112 <okeke98@gmail.com>
2025-04-14 04:58:16 +0100

    feat: video categories created

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,21 @@
+import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
-import { PageClient } from "./client";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
+export const dynamic = "force-dynamic";
 
-export default  function Home() {
-  void trpc.hello.prefetch({text: "Francis"})
+interface PageProps {
+  searchParams: Promise<{
+    categoryId?: string;
+  }>
+}
+const Page = async ({ searchParams }: PageProps) => {
+  const { categoryId } = await searchParams;
+  void trpc.categories.getMany.prefetch();
   return (
     <HydrateClient>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary fallback={<p>Something went wrong</p>}>
-          <PageClient />
-        </ErrorBoundary>
-      </Suspense>
+      <HomeView categoryId={categoryId} />
     </HydrateClient>
   );
 }
+
+export default Page;
