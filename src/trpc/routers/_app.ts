044432e155896fea100bdfb0b6a5b64b2255ba67
okekefrancis112 <okeke98@gmail.com>
2025-04-14 01:52:36 +0100

    feat: trpc configuration and rate limiting added

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
-import { TRPCError } from '@trpc/server';
+import { protectedProcedure, createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       }),
     )
     .query((opts) => {
-      throw new TRPCError({ code: 'BAD_REQUEST' })
+      console.log({ dbUser: opts.ctx.user });
+
       return {
         greeting: `hello ${opts.input.text}`,
       };
