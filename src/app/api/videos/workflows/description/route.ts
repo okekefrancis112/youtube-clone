bcb98f63f38940e787850d9c3841e882c9194586
okekefrancis112 <okeke98@gmail.com>
2025-11-24 15:04:12 +0100

    ai thumbnails

--- src/app/api/videos/workflows/description/route.ts
+++ src/app/api/videos/workflows/description/route.ts
@@ -3,13 +3,14 @@ import { videos } from "@/db/schema";
 import { serve } from "@upstash/workflow/nextjs"
 import { and, eq } from "drizzle-orm";
 
-const DESCRIPTION_SYSTEM_PROMPT = `Your task is to summarize the transcript of a video. Please follow these guidelines:
+const DESCRIPTION_SYSTEM_PROMPT =
+    `Your task is to summarize the transcript of a video. Please follow these guidelines:
 
-- Be brief. Condense the content into a summary that captures the key points and main ideas without losing important details.
-- Avoid jargon or overly complex language unless necessary for the context.
-- Focus on the most critical information, ignoring filler, repetitive statements, or irrelevant tangents.
-- ONLY return the summary, no other text, annotations, or comments.
-- Aim for a summary that is 3-5 sentences long and no more than 200 characters.`;
+    - Be brief. Condense the content into a summary that captures the key points and main ideas without losing important details.
+    - Avoid jargon or overly complex language unless necessary for the context.
+    - Focus on the most critical information, ignoring filler, repetitive statements, or irrelevant tangents.
+    - ONLY return the summary, no other text, annotations, or comments.
+    - Aim for a summary that is 3-5 sentences long and no more than 200 characters.`;
 
 interface InputType {
     userId: string;
