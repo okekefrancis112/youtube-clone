e9b73aee1bf69489f89613642ff0e0d54f0fb8c1
okekefrancis112 <okeke98@gmail.com>
2025-11-30 01:09:21 +0100

    video views

--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -15,14 +15,14 @@ export const VideoTopRow = ({ video }: VideoTopRowProps) => {
     const compactViews = useMemo(() => {
         return Intl.NumberFormat("en", {
             notation: "compact"
-        }).format(124596868);
-    }, []);
+        }).format(video.viewCount);
+    }, [video.viewCount]);
 
     const expandedViews = useMemo(() => {
         return Intl.NumberFormat("en", {
             notation: "standard"
-        }).format(124596868);
-    }, []);
+        }).format(video.viewCount);
+    }, [video.viewCount]);
 
     const compactDate = useMemo(() => {
         return formatDistanceToNow(video.createdAt, { addSuffix: true });
