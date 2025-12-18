8e4e85bcbd8cefb06c44414547f57a9732ffb550
okekefrancis112 <okeke98@gmail.com>
2025-12-18 10:21:35 +0100

    banner-upload

--- src/modules/users/ui/sections/user-page-banner.tsx
+++ src/modules/users/ui/sections/user-page-banner.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Edit2Icon } from "lucide-react";
 import { Skeleton } from "@/components/ui/skeleton";
+import { BannerUploadModal } from "../components/banner-upload-modal";
+import { useState } from "react";
 
 interface UserPageBannerProps {
     user: UserGetOneOutput;
@@ -17,9 +19,15 @@ export const UserPageBannerSkeleton = () => {
 
 export const UserPageBanner = ({ user }: UserPageBannerProps) => {
     const { userId } = useAuth();
+    const [isBannerUploadModalOpen, setIsBannerUploadModalOpen] = useState(false);
+
     return (
         <div className="relative group">
-            {/* TODO: Add upload banner modal */}
+            <BannerUploadModal
+                userId={user.id}
+                open={isBannerUploadModalOpen}
+                onOpenChange={setIsBannerUploadModalOpen}
+            />
             <div
                 className={cn(
                     "w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl",
@@ -33,6 +41,7 @@ export const UserPageBanner = ({ user }: UserPageBannerProps) => {
             >
                 {user.clerkId === userId && (
                     <Button
+                        onClick={() => setIsBannerUploadModalOpen(true)}
                         type="button"
                         size="icon"
                         className="absolute top-4 right-4 rounded-full bg-black/50 hover:bg-black/50 opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity duration-300"
